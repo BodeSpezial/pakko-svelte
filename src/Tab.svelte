20c231da2f0755cc1df0b8db04c88b8fea0de684
BodeSpezial <konstantin.bode@outlook.com>
2021-06-03 13:40:38 +0200

    create tab component for tabcontainer

---
+++ src/Tab.svelte
@@ -0,0 +1,21 @@
+<script>
+export let enabled;
+</script>
+
+<style>
+    .tab {
+        display: none;
+    }
+
+    .tab.enabled {
+        display: block;
+    }
+
+    button {
+        outline: none;
+    }
+</style>
+
+<div class="tab rounded-b-lg bg-green-50 p-2" class:enabled={enabled}>
+    <slot />
+</div>
